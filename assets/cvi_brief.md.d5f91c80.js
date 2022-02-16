bd6456178e22ad41b228f3155ddb20cc4ad5cf43
opilane <opilane@KLASSB306-22.local>
2022-02-16 14:00:41 +0200

    deploy

---
+++ assets/cvi_brief.md.d5f91c80.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.e19a5390.js";const f='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"cvi/brief.md","lastUpdated":1644999451252}',r={};function o(s,c,n,p,i,_){return a(),t("div")}var l=e(r,[["render",o]]);export{f as __pageData,l as default};
---
+++ assets/pilt4.7c165059.js
@@ -0,0 +1,1 @@
+var s="/123/assets/pilt4.783582e5.png";export{s as _};
